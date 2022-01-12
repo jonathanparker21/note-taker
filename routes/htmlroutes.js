358625e8406ab094a5115d4d8c6ff741a89b2889
Jonathan Parker <jprkr21@gmail.com>
2022-01-11 18:30:15 -0600

    creating htmlroutes file

---
+++ routes/htmlroutes.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
+
+// route to send user input to notes.html
+router.get('/api/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/notes.html'))
+});
+
+// route to send user to index.html
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+  });
+
+module.exports = router;
