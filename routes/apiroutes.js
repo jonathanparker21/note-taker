9d18cd8c0b00094c8c7b98061a8a04de0804683e
Jonathan Parker <jprkr21@gmail.com>
2022-01-11 19:29:42 -0600

    working on apiroutes

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -4,49 +4,18 @@ const dataBase = require('../db/db.json')
 const fs = require('fs');
 const { json } = require('express/lib/response');
 
-// route to retrieve notes from the db
-router.get('/', (req, res) => {
-    res.json(JSON.parse(fs.readFileSync("./db/db.json", "utf8")))
-}
-);
-
-// POST Route for submitting feedback
-router.post('/', (req, res) => {
-console.info(`${req.method} request received to add a note`);
-  const { id, title, text } = req.body;
-
-  if (title && text) {
-    const newNote = {
-      title,
-      text,
-      id: uuidv4(),
-    };
-    
-    const noteString = JSON.stringify(newNote)
-
-    fs.readFile('../db/db.json', 'utf8', (err, noteString) => {
-        if (err) {
-            console.log(err)
-        } else {
-            let notes = JSON.parse(noteString)
-            notes.push(newNote)
-        }
-
-        fs.writeFile('../db/db.json', JSON.stringify(notes), (err) => {
-            console.log(err)
-            res.json(notes)
-        })
+module.exports = function (router) {  
+  router.get("/notes", function (req,res){
+    res.json(dataBase)
+  })
+  
+  router.post("/notes", function (req,res){
+    req.body.id = uuidv4()
+    dataBase.push(req.body)
+    fs.writeFile("../db/db.json", JSON.stringify(dataBase), function(err){
+      if(err) {
+        throw error
+      }
+      res.json("saved")
     })
-
-    const response = {
-      status: 'success',
-      body: newNote,
-    };
-
-    res.json(response);
-  } else {
-    res.json('Error in posting note');
   }
-});
-
-module.exports = router;
