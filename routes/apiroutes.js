932a1601e18cb2c5c164434c4f323e5ced85acdb
Jonathan Parker <jprkr21@gmail.com>
2022-01-16 16:44:03 -0600

    finalizing apiroutes

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,22 +1,45 @@
 const router = require('express').Router();
 const { v4: uuidv4 } = require('uuid');
-const dataBase = require('../db/db.json')
 const fs = require('fs');
-const { json } = require('express/lib/response');
 
-module.exports = function (router) {  
-  router.get("/notes", function (req,res) {
-    res.json(dataBase)
-  })
-  
-  router.post("/notes", function (req,res) {
-    req.body.id = uuidv4()
-    dataBase.push(req.body)
-    fs.writeFile("../db/db.json", JSON.stringify(dataBase), function(err){
-      if(err) {
-        throw error
+router.get('/', (req, res) => {
+  res.json(JSON.parse(fs.readFileSync('./db/db.json', 'utf-8')))
+})
+
+router.post('/', (req, res) => {
+
+  const { title, text, id } = req.body
+
+  if (title && text) {
+    const newEntry = {
+      title,
+      text,
+      id: uuidv4()
+    }
+
+    let notes = JSON.stringify(newEntry)
+    fs.readFile('./db/db.json', 'utf-8', (err, notes) => {
+      if (err) {
+        console.log('There was an error reading the file.' + err)
       }
-      res.json("saved")
+
+      let binder = JSON.parse(notes)
+      binder.push(newEntry)
+
+
+      fs.writeFile('./db/db.json', JSON.stringify(binder), (err) => {
+        if (err) {
+          console.log('There was an error writing to the db.json.' + err)
+        } else {
+          console.log('New entry successfully written to the db.')
+        }
+
+        res.json(binder)
+
+      })
+
     })
-  })
-}
+  }
+})
+
+module.exports = router;
